7a137da2dd882d0d42736686d7bb8b3b5fc67d54
yeyun0423 <pet2ytty@naver.com>
2024-11-26 23:36:08 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+
+import Introduce from "./pages/Introduce";
+import AboutUs from "./pages/AboutUs";
+import ChatBot from "./pages/ChatBot";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/pages/Introduce" element={<Introduce />} />
+        <Route path="/pages/AboutUs" element={<AboutUs />} />
+        <Route path="/pages/ChatBot" element={<ChatBot />} />
+        <Route path="/pages/Login" element={<Login />} />
+        <Route path="/pages/Signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
