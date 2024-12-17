b4633de12ef135c976ae177cfb9d883827846029
한은정 <141193305+onesilver04@users.noreply.github.com>
2024-12-17 23:54:21 +0900

    feat: MyPage 디자인 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// // import Header from "./components/Header";
+
+// import Introduce from "./pages/Introduce";
+// import AboutUs from "./pages/AboutUs";
+// import ChatBot from "./pages/ChatBot";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import MyPage from "./pages/MyPage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <MyPage />
+//       <Routes>
+//         <Route path="/pages/Introduce" element={<Introduce />} />
+//         <Route path="/pages/AboutUs" element={<AboutUs />} />
+//         <Route path="/pages/ChatBot" element={<ChatBot />} />
+//         <Route path="/pages/Login" element={<Login />} />
+//         <Route path="/pages/Signup" element={<Signup />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Header from "./components/Header";
 
 import Introduce from "./pages/Introduce";
 import AboutUs from "./pages/AboutUs";
 import ChatBot from "./pages/ChatBot";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
     <Router>
-      <Login />
       <Routes>
+        <Route path="/" element={<MyPage />} /> {/* 기본 경로로 설정 */}
         <Route path="/pages/Introduce" element={<Introduce />} />
         <Route path="/pages/AboutUs" element={<AboutUs />} />
         <Route path="/pages/ChatBot" element={<ChatBot />} />
