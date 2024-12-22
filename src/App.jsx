8a2e4cc47ab447547436507d77c1807659ef4c7c
한은정 <141193305+onesilver04@users.noreply.github.com>
2024-12-23 03:16:00 +0900

    feat:PromisePage1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// // import Header from "./components/Header";
-
-// import Introduce from "./pages/Introduce";
-// import AboutUs from "./pages/AboutUs";
-// import ChatBot from "./pages/ChatBot";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import MyPage from "./pages/MyPage";
-
-// function App() {
-//   return (
-//     <Router>
-//       <MyPage />
-//       <Routes>
-//         <Route path="/pages/Introduce" element={<Introduce />} />
-//         <Route path="/pages/AboutUs" element={<AboutUs />} />
-//         <Route path="/pages/ChatBot" element={<ChatBot />} />
-//         <Route path="/pages/Login" element={<Login />} />
-//         <Route path="/pages/Signup" element={<Signup />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -35,17 +7,21 @@ import ChatBot from "./pages/ChatBot";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MyPage from "./pages/MyPage";
+import Promise1 from "./pages/Promise1";
+import PromiseSuccess from "./pages/PromiseSuccess";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MyPage />} /> {/* 기본 경로로 설정 */}
+        <Route path="/" element={<Promise1 />} /> {/* 기본 경로로 설정 */}
         <Route path="/pages/Introduce" element={<Introduce />} />
         <Route path="/pages/AboutUs" element={<AboutUs />} />
         <Route path="/pages/ChatBot" element={<ChatBot />} />
         <Route path="/pages/Login" element={<Login />} />
         <Route path="/pages/Signup" element={<Signup />} />
+        <Route path="/pages/Mypage" element={<MyPage />} />
+        <Route path="/pages/PromiseSuccess" element={<PromiseSuccess />} />
       </Routes>
     </Router>
   );
