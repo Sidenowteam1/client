45238a7ef923d51a8426df6656fa253e190fb85e
한은정 <141193305+onesilver04@users.noreply.github.com>
2024-12-31 15:46:45 +0900

    feat:Footer 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Footer from "./components/Footer";
 import Introduce from "./pages/Introduce";
 import AboutUs from "./pages/AboutUs";
 import ChatBot from "./pages/ChatBot";
@@ -15,22 +16,26 @@ import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} /> {/* 기본 경로로 설정 */}
-        {/* <Route path="/pages/MainPage" element={<MainPage />} /> */}
-        <Route path="/pages/Introduce" element={<Introduce />} />
-        <Route path="/pages/AboutUs" element={<AboutUs />} />
-        <Route path="/pages/ChatBot" element={<ChatBot />} />
-        <Route path="/pages/Login" element={<Login />} />
-        <Route path="/pages/Signup" element={<Signup />} />
-        <Route path="/pages/Mypage" element={<MyPage />} />
-        <Route path="/pages/Promise1" element={<Promise1 />} />
-        <Route path="/pages/PromiseSuccess" element={<PromiseSuccess />} />
-        <Route path="/pages/PromiseMember" element={<Member />} />
-        <Route path="/pages/MyPromise" element={<MyPromsie />} />
-      </Routes>
-    </Router>
+    <div className="app">
+      <Router>
+        <div className="router-content">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/pages/Introduce" element={<Introduce />} />
+            <Route path="/pages/AboutUs" element={<AboutUs />} />
+            <Route path="/pages/ChatBot" element={<ChatBot />} />
+            <Route path="/pages/Login" element={<Login />} />
+            <Route path="/pages/Signup" element={<Signup />} />
+            <Route path="/pages/Mypage" element={<MyPage />} />
+            <Route path="/pages/Promise1" element={<Promise1 />} />
+            <Route path="/pages/PromiseSuccess" element={<PromiseSuccess />} />
+            <Route path="/pages/PromiseMember" element={<Member />} />
+            <Route path="/pages/MyPromise" element={<MyPromsie />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../public/css/Main.css";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <img src="..\src\images\logo.png" alt="로고" className="logo" />
+    </div>
+  );
 };
 
 export default Footer;
